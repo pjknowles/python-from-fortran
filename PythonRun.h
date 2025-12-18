e46bfd317838e438a34b81dfe83b129e94a1c303
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-12-18 23:12:19 +0000

    class refactor

---
+++ PythonRun.h
@@ -0,0 +1,41 @@
+#ifndef PYTHON_FROM_FORTRAN_PYTHONRUN_HPP
+#define PYTHON_FROM_FORTRAN_PYTHONRUN_HPP
+
+#include <string>
+
+class PythonRun
+{
+public:
+    PythonRun(std::string script = "", std::string path = "");
+
+    ~PythonRun();
+
+    void add_path(std::string path);
+
+    void load_module(std::string name);
+
+    void run_string(std::string str);
+
+    /* @brief Run a python function that expects zero or 1 string arguments and returns a string
+     *
+     * @param function_name
+     * @param module
+     * @param argument
+     * @param path
+     * @return
+     */
+    std::string run_str_str_function(std::string function_name, std::string module, std::string argument = "",
+                                     std::string path = "");
+
+
+    const std::string out() { return stream("stdout"); }
+    const std::string err() { return stream("stderr"); }
+    const std::string& script() { return script_; }
+
+private:
+    std::string stream(char* name);
+    std::string script_;
+};
+
+
+#endif //PYTHON_FROM_FORTRAN_PYTHONRUN_HPP
--- main.cpp
+++ main.cpp
@@ -2,92 +2,9 @@
 #include <Python.h>
 #include <filesystem>
 #include <chrono>
+#include "PythonRun.h"
 
-class PythonRun
-{
-public:
-    PythonRun(std::string script = "", std::string path = "")
-    {
-        Py_Initialize();
-        if (!path.empty()) add_path(path);
-        PyRun_SimpleString(
-            "import sys\n\
-class CatchOutErr:\n\
-    def __init__(self):\n\
-        self.value = ''\n\
-    def write(self, txt):\n\
-        self.value += txt\n\
-sys.stdout = CatchOutErr()\n\
-sys.stderr = CatchOutErr()\n\
-");
-        if (!script.empty()) run_string(script);
-    }
 
-    ~PythonRun()
-    {
-        Py_Finalize();
-    }
-
-    void add_path(std::string path)
-    {
-        PyRun_SimpleString((std::string{"import sys;sys.path.append('"}+path+"')").c_str());
-    }
-
-    void load_module(std::string name)
-    {
-        // PyRun_SimpleString((std::string{"import "}+name).c_str());
-        auto nam = PyUnicode_FromString(name.c_str());
-        auto load_module = PyImport_Import(nam);
-    }
-
-    void run_string(std::string str)
-    {
-        PyRun_SimpleString(str.c_str());
-        if (!this->script_.empty())
-            this->script_ += "\n";
-        this->script_ += str;
-    }
-
-    /* @brief Run a python function that expects zero or 1 string arguments and returns a string
-     *
-     * @param function_name
-     * @param module
-     * @param argument
-     * @param path
-     * @return
-     */
-    std::string run_str_str_function(std::string function_name, std::string module, std::string argument = "",
-                                     std::string path = "")
-    {
-        if (!path.empty()) add_path(path);
-        auto nam = PyUnicode_FromString(module.c_str());
-        auto load_module = PyImport_Import(nam);
-        auto func = PyObject_GetAttrString(load_module, function_name.c_str());
-
-        auto callfunc = PyObject_CallObject(func, argument.empty()
-                                                      ? NULL
-                                                      : PyTuple_Pack(1, PyUnicode_FromString(argument.c_str())));
-        return std::string{PyBytes_AS_STRING(PyUnicode_AsEncodedString(PyObject_Str(callfunc),"utf-8","~E~"))};
-    }
-
-    const std::string out() { return stream("stdout"); }
-    const std::string err() { return stream("stderr"); }
-    const std::string& script() { return script_; }
-
-private:
-    std::string stream(char* name)
-    {
-        PyObject* catcher = PyObject_GetAttrString(PyImport_ImportModule("sys"), name);
-        PyErr_Print();
-        PyObject* output = PyObject_GetAttrString(catcher, "value");
-        auto py_bytes_as_string = std::string{PyBytes_AS_STRING(PyUnicode_AsEncodedString(output,"utf-8","~E~"))};
-        if (!py_bytes_as_string.empty() && py_bytes_as_string.back() == '\n')
-            py_bytes_as_string.pop_back();
-        return py_bytes_as_string;
-    }
-
-    std::string script_;
-};
 
 auto load_and_call_python_function(char* person = NULL)
 {
